3b61cf8e56a36a8776b671c655e971a458b69dfe
Prince Issar <82021741+PrinceIssar@users.noreply.github.com>
2021-05-25 19:55:02 +0200

    added files

--- form-view.php
+++ form-view.php
@@ -7,9 +7,53 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
           rel="stylesheet"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
     <title>Order food & drinks</title>
 </head>
 <body>
+
+
+<!--Carousel Starts-->
+
+<div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="img/burger.jpeg" alt="burger">
+            </div>
+
+            <div class="item">
+                <img src="img/menu.jpeg" alt="menu">
+            </div>
+
+            <div class="item">
+                <img src="img/menu1.jpeg" alt="menu1">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+<!--Carousel Ends-->
 <div class="container">
     <h1>Order food in restaurant "the Personal Ham Processors"</h1>
     <nav>
@@ -22,36 +66,42 @@
             </li>
         </ul>
     </nav>
+
     <form method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control"/>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $email?>">
             </div>
             <div></div>
         </div>
 
         <fieldset>
             <legend>Address</legend>
-
+            <p><span class="error">* required field</span></p>
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <label for="street">Street: *</label>
+                    <input type="text" name="street" id="street" class="form-control"  value="<?php echo $street ?>">
+                    <span class="error"><?php echo $streetErr;?></span>
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                    <label for="streetnumber">Street number: *</label>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $streetNumber?>">
+                    <span class="error"><?php echo $streetNumErr;?></span>
+
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+                    <label for="city">City: *</label>
+                    <input type="text" id="city" name="city" class="form-control"  value="<?php echo $city?>">
+                    <span class="error"> <?php echo $cityErr;?></span>
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                    <label for="zipcode">Zipcode *</label>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $zipCode?>">
+                    <span class="error"> <?php echo $zipcodeErr;?></span>
                 </div>
             </div>
         </fieldset>
@@ -60,16 +110,18 @@
             <legend>Products</legend>
             <?php foreach ($products AS $i => $product): ?>
                 <label>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
+                    <input type="checkbox"  value= <?php echo $product['price']?> name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
                     &euro; <?php echo number_format($product['price'], 2) ?></label><br />
             <?php endforeach; ?>
+
+
         </fieldset>
-        
+
         <label>
-            <input type="checkbox" name="express_delivery" value="5" /> 
-            Express delivery (+ 5 EUR) 
+            <input type="checkbox" name="express_delivery" value="5" />
+            Express delivery (+ 5 EUR)
         </label>
-            
+
         <button type="submit" class="btn btn-primary">Order!</button>
     </form>
 
@@ -81,5 +133,6 @@
         text-align: center;
     }
 </style>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 //we are going to use session variables so we need to enable sessions
 session_start();
 
-function whatIsHappening() {
+function whatIsHappening()
+{
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -15,9 +16,11 @@ function whatIsHappening() {
     echo '<h2>$_SESSION</h2>';
     var_dump($_SESSION);
 }
+//uncomment this line below to get the information to check values.
+//whatIsHappening();
 
-//your products with their price.
-$products = [
+//your products with their price. changed the names so they are easier to call on
+$sandwiches = [
     ['name' => 'Club Ham', 'price' => 3.20],
     ['name' => 'Club Cheese', 'price' => 3],
     ['name' => 'Club Cheese & Ham', 'price' => 4],
@@ -25,13 +28,151 @@ $products = [
     ['name' => 'Club Salmon', 'price' => 5]
 ];
 
-$products = [
+$drinks = [
     ['name' => 'Cola', 'price' => 2],
     ['name' => 'Fanta', 'price' => 2],
     ['name' => 'Sprite', 'price' => 2],
     ['name' => 'Ice-tea', 'price' => 3],
 ];
+//gets the data food, being the link between drink/food.
+//shows different arrays depending on food.
+//also checks if food has a value, if not it's the default page and it shows sandwiches.
 
+
+$food = $_GET['food'];
+if (!isset($food)) {
+    $products = $sandwiches;
+} else if ($food == 0) {
+    $products = $drinks;
+} else {
+    $products = $sandwiches;
+}
 $totalValue = 0;
 
+
+//obtaining all the data from post.
+//declaring the error messages.
+$zipcodeErr = $streetErr = $streetNumErr = $cityErr = "";
+
+
+//actions to do once the user pressed post.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //checks if the zipcode is filled in
+    if (empty($_POST["zipcode"])) {
+        $zipcodeErr = "zipcode is required";
+        //checks if the zipcode has only numbers in it.
+    } else if (is_numeric($_POST['zipcode'])) {
+
+        $zipCode = test_input($_POST["zipcode"]);
+        //saves the content into the session variable.
+        $_SESSION['zipCodeSes'] = $zipCode;
+
+    } else {
+        //error message in case is_numeric returns false.
+        $zipcodeErr = "zipcode must be a number";
+    }
+    //checks if streetnumber is filled in or not.
+    if (empty($_POST["streetnumber"])) {
+        $streetNumErr = "street number is required";
+        //checks if streetnumber is solely numbers, might have issues people with the same house number.
+    } else if (is_numeric($_POST["streetnumber"])) {
+        $streetNumber = test_input($_POST["streetnumber"]);
+        $_SESSION['streetnumber'] = test_input($_POST["streetnumber"]);
+
+    } else {
+        //error message in case a letter occured.
+        $streetNumErr = "street number must be a number";
+    }
+
+    //checks if city is empty or filled in.
+    if (empty($_POST["city"])) {
+        $cityErr = " city is required";
+    } else {
+        //writes post data into a variable, that variable is written into the session.
+        $city = test_input($_POST["city"]);
+        $_SESSION['city'] = $city;
+    }
+    //checks if street input is empty or filled in.
+    if (empty($_POST["street"])) {
+        $streetErr = " street is required";
+    } else {
+        //writes post data into a variable, that variable is written into the session.
+        $street = test_input($_POST["street"]);
+        $_SESSION['streetSes'] = $street;
+    }
+    //checks if there is an e-mail listed, verifies if it's a valid e-mail address.
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+        filter_var($email, FILTER_SANITIZE_ENCODED) ;
+        //saves the e-mail into the session.
+        $_SESSION['email'] = $email;
+    } else {
+        //only echoes if the e-mail is wrong as it was not 100% required according to my reading comprehension.
+        echo "error, invalid e-mail";
+    }
+
+    //changing time depending if express delivery was checked or not
+
+    if (isset($_POST['express_delivery'])) {
+        $newTime=date("H:i", strtotime('+45 minutes'));
+        $time = ucfirst("your food will arrive at ").$newTime;
+        //also adding price to the total value;
+        $totalValue += 5;
+    }else {
+        $newTime = date("H:i", strtotime('+2 hours'));
+        $time= ucfirst("your food will arrive at ").$newTime;
+    }
+
+
+
+//fixed this by making it compare all separately, otherwise it assigns the value.
+    if ($zipcodeErr == "" && $streetErr == "" && $streetNumErr == "" && $cityErr == "") {
+        echo strtoupper("your order has been sent.<br>") . $time.".";
+        //moved foreach into here to see if total value will stop updating even upon errors
+        //check which checkboxes are checked
+        //var_dump($_POST['products']);
+        // for each of these checked checkboxes, add the product price to totalValue.
+        $checkboxes = isset($_POST['products']) ? $_POST['products']: array();
+        foreach ($checkboxes as $value) {
+            //value only shows the price, products shows the word array, products['name returns empty string];
+
+            $totalValue +=$value;
+        }
+    }
+    $_SESSION['totalses'] += $totalValue;
+
+}
+//First check for post, then check for session, not opposite.
+//section to check if there is session data, if so add it to the variable.
+if (isset($_SESSION['streetnumber'])) {
+    $streetNumber = test_input($_SESSION['streetnumber']);
+}
+if (isset($_SESSION['email'])) {
+    $email = $_SESSION['email'];
+}
+if (isset($_SESSION['streetSes'])) {
+    $street = $_SESSION['streetSes'];
+}
+if (isset($_SESSION['city'])) {
+    $city = $_SESSION['city'];
+}
+if (isset($_SESSION['zipCodeSes'])) {
+    $zipCode = $_SESSION['zipCodeSes'];
+}
+if (isset($_SESSION['totalses'])) {
+    $totalValue = $_SESSION['totalses'];
+}
+function test_input($data)
+{
+    //if understood correctly trim removes special characters, but can also be specified to trim certain letters in a string.
+    $data = trim($data);
+    //removes any slashes that might occur.
+    $data = stripslashes($data);
+    //Convert the predefined characters "<" (less than) and ">" (greater than) to HTML entities.
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+//required to link both files, was already included in code.
 require 'form-view.php';
+
